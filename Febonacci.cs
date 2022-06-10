93a43873f9437d356c8124bea5b3bf5eaad0f2b8
Sujit8820 <kadamsujit8820@gmail.com>
2022-06-10 18:49:13 +0530

    [Sujit]Added: Febonacchi file

---
+++ Febonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Problems;
+class Febonacci
+{
+    public void Febonacci_Series()
+    {
+        Console.WriteLine("Enter a number");
+        int series = Convert.ToInt32(Console.ReadLine());
+        int previousValue = 0;
+        int value = 1;
+        int ans = 0;
+
+        for (int i = 1; i <= series; i++)
+        {
+            ans = previousValue + value;
+            previousValue = value;
+            value = ans;
+
+            Console.WriteLine(ans);
+        }
+
+    }
+}
+
